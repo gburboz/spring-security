e969069e95fca9188955c1fe6ab2e5f595e34749
Gladwin Burboz <8951010+gburboz@users.noreply.github.com>
2018-11-03 18:24:50 -0400

    DefaultBearerTokenResolver issue #5668, err - form w/ tkn in query param

--- oauth2/oauth2-resource-server/src/main/java/org/springframework/security/oauth2/server/resource/web/DefaultBearerTokenResolver.java
+++ oauth2/oauth2-resource-server/src/main/java/org/springframework/security/oauth2/server/resource/web/DefaultBearerTokenResolver.java
@@ -16,6 +16,11 @@
 
 package org.springframework.security.oauth2.server.resource.web;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
@@ -25,7 +30,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.server.resource.BearerTokenError;
 import org.springframework.security.oauth2.server.resource.BearerTokenErrorCodes;
-import org.springframework.util.StringUtils;
 
 /**
  * The default {@link BearerTokenResolver} implementation based on RFC 6750.
@@ -36,7 +40,17 @@ import org.springframework.util.StringUtils;
  */
 public final class DefaultBearerTokenResolver implements BearerTokenResolver {
 
-	private static final Pattern authorizationPattern = Pattern.compile("^Bearer (?<token>[a-zA-Z0-9-._~+/]+)=*$");
+	private static final BearerTokenError BEARER_TOKEN_ERROR_MULTIPLE = new BearerTokenError(
+			BearerTokenErrorCodes.INVALID_REQUEST, HttpStatus.BAD_REQUEST,
+			"Found multiple bearer tokens in the request", "https://tools.ietf.org/html/rfc6750#section-3.1");
+
+	private static final BearerTokenError BEARER_TOKEN_ERROR_MALFORMED = new BearerTokenError(
+			BearerTokenErrorCodes.INVALID_TOKEN, HttpStatus.UNAUTHORIZED, "Bearer token is malformed",
+			"https://tools.ietf.org/html/rfc6750#section-3.1");
+
+	private static final String ACCESS_TOKEN_PARAM_NAME = "access_token";
+
+	private static final Pattern AUTHORIZATION_PATTERN = Pattern.compile("^Bearer (?<token>[a-zA-Z0-9-._~+/]+)=*$");
 
 	private boolean allowFormEncodedBodyParameter = false;
 
@@ -48,21 +62,8 @@ public final class DefaultBearerTokenResolver implements BearerTokenResolver {
 	@Override
 	public String resolve(HttpServletRequest request) {
 		String authorizationHeaderToken = resolveFromAuthorizationHeader(request);
-		String parameterToken = resolveFromRequestParameters(request);
-		if (authorizationHeaderToken != null) {
-			if (parameterToken != null) {
-				BearerTokenError error = new BearerTokenError(BearerTokenErrorCodes.INVALID_REQUEST,
-						HttpStatus.BAD_REQUEST,
-						"Found multiple bearer tokens in the request",
-						"https://tools.ietf.org/html/rfc6750#section-3.1");
-				throw new OAuth2AuthenticationException(error);
-			}
-			return authorizationHeaderToken;
-		}
-		else if (parameterToken != null && isParameterTokenSupportedForRequest(request)) {
-			return parameterToken;
-		}
-		return null;
+		validate(request, authorizationHeaderToken);
+		return authorizationHeaderToken != null ? authorizationHeaderToken : resolveFromRequestParameters(request);
 	}
 
 	/**
@@ -85,17 +86,24 @@ public final class DefaultBearerTokenResolver implements BearerTokenResolver {
 		this.allowUriQueryParameter = allowUriQueryParameter;
 	}
 
+	private void validate(final HttpServletRequest request, final String authorizationHeaderToken) {
+		if (hasMultipleAccessTokens(request, authorizationHeaderToken)) {
+			throw new OAuth2AuthenticationException(BEARER_TOKEN_ERROR_MULTIPLE);
+		}
+		if (this.allowFormEncodedBodyParameter) {
+			if (!this.allowUriQueryParameter && isTokenInUriQueryParameter(request)) {
+				throw new OAuth2AuthenticationException(BEARER_TOKEN_ERROR_MALFORMED);
+			}
+		}
+	}
+
 	private static String resolveFromAuthorizationHeader(HttpServletRequest request) {
-		String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
-		if (StringUtils.hasText(authorization) && authorization.startsWith("Bearer")) {
-			Matcher matcher = authorizationPattern.matcher(authorization);
+		final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+		if (authorization != null && authorization.startsWith("Bearer")) {
+			final Matcher matcher = AUTHORIZATION_PATTERN.matcher(authorization);
 
 			if (!matcher.matches()) {
-				BearerTokenError error = new BearerTokenError(BearerTokenErrorCodes.INVALID_TOKEN,
-						HttpStatus.UNAUTHORIZED,
-						"Bearer token is malformed",
-						"https://tools.ietf.org/html/rfc6750#section-3.1");
-				throw new OAuth2AuthenticationException(error);
+				throw new OAuth2AuthenticationException(BEARER_TOKEN_ERROR_MALFORMED);
 			}
 
 			return matcher.group("token");
@@ -103,25 +111,41 @@ public final class DefaultBearerTokenResolver implements BearerTokenResolver {
 		return null;
 	}
 
-	private static String resolveFromRequestParameters(HttpServletRequest request) {
-		String[] values = request.getParameterValues("access_token");
-		if (values == null || values.length == 0)  {
+	private String resolveFromRequestParameters(HttpServletRequest request) {
+		// By this point there is either zero or one value
+		final String value = request.getParameter(ACCESS_TOKEN_PARAM_NAME);
+		if (value != null && isParameterTokenSupportedForRequest(request)) {
+			return value;
+		} else {
 			return null;
 		}
-
-		if (values.length == 1) {
-			return values[0];
-		}
-
-		BearerTokenError error = new BearerTokenError(BearerTokenErrorCodes.INVALID_REQUEST,
-				HttpStatus.BAD_REQUEST,
-				"Found multiple bearer tokens in the request",
-				"https://tools.ietf.org/html/rfc6750#section-3.1");
-		throw new OAuth2AuthenticationException(error);
 	}
 
 	private boolean isParameterTokenSupportedForRequest(HttpServletRequest request) {
 		return ((this.allowFormEncodedBodyParameter && "POST".equals(request.getMethod()))
 				|| (this.allowUriQueryParameter && "GET".equals(request.getMethod())));
 	}
+
+	private boolean hasMultipleAccessTokens(final HttpServletRequest request, final String authorizationHeaderToken) {
+		final String[] parameterTokens = request.getParameterValues(ACCESS_TOKEN_PARAM_NAME);
+		final boolean multipleParameterTokensPresent = parameterTokens != null && parameterTokens.length > 1;
+		final boolean bothParameterTokenAndHeaderPresent = parameterTokens != null && authorizationHeaderToken != null;
+		return multipleParameterTokensPresent || bothParameterTokenAndHeaderPresent;
+	}
+
+	private boolean isTokenInUriQueryParameter(final HttpServletRequest request) {
+		final String queryString = request.getQueryString();
+		return queryString != null && Arrays.stream(queryString.split("&"))
+				.map(param -> urlDecodeUTF8(param.split("=")[0]))
+				.anyMatch(Predicate.isEqual(ACCESS_TOKEN_PARAM_NAME));
+	}
+
+	private String urlDecodeUTF8(final String value) {
+		try {
+			return URLDecoder.decode(value, StandardCharsets.UTF_8.toString());
+		} catch (UnsupportedEncodingException e) {
+			return value;
+		}
+	}
+
 }
